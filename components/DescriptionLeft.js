03762fa73d02f5cd50084855e50c8c3c3d4ebfc9
jcamara <camarajoao1@gmail.com>
2023-05-29 16:30:31 -0700

    commit from previous project

---
+++ components/DescriptionLeft.js
@@ -0,0 +1,7 @@
+function DescriptionLeft({ sidebarLeft }) {
+  return (
+    <div className={sidebarLeft ? 'sidebar-left active' : 'sidebar-left'}>DescriptionLeft</div>
+  )
+}
+
+export default DescriptionLeft
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.scss';
+import '../styles/Header.scss';
+import '../styles/Home.scss';
+import '../styles/Sidebars.scss';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
